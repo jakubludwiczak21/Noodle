2227e6bd20a706f8f913e9f98e455d6527fea6f3
jakubludwiczak21 <163825948+jakubludwiczak21@users.noreply.github.com>
2024-04-05 19:05:02 +0200

    Add files via upload

---
+++ stronaaa/script.js
@@ -0,0 +1,13 @@
+/*document.addEventListener('DOMContentLoaded', function () {
+    const dropdownBtns = document.querySelectorAll('.dropdown-btn');
+  
+    dropdownBtns.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+        const dropdownContent = this.nextElementSibling;
+        dropdownContent.style.display === 'block'
+          ? (dropdownContent.style.display = 'none')
+          : (dropdownContent.style.display = 'block');
+      });
+    });
+  });
+  */
