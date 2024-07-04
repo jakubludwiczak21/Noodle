e6f729be15d6171ddf9152685c5749563e272ac2
Mikołaj Janczak <mikolaj.janczaq@gmail.com>
2024-07-04 18:36:12 +0200

    Odczytywanie kodu testu z linku

--- Strona/html/panel_ucznia/dolacz_kod.php
+++ Strona/html/panel_ucznia/dolacz_kod.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-//echo "Welcome, " . htmlspecialchars($_SESSION['username']) . "!";
+$kod = isset($_GET['kod']) ? htmlspecialchars($_GET['kod']) : '';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,47 +26,43 @@ if (!isset($_SESSION['user_id'])) {
   <div class="container" id="con">
 
     <div class="sidebar" id="menu">
-      
       <ul>
         <h2>Menu</h2>
         <li><a href="strona_glowna.php">Strona główna</a></li>
-        <li ><a href="dolacz_kod.php" class="aktualna-strona">Dołącz do testu</a></li>
-        <li><a href="moje_testy.php" >Moje testy</a></li>
+        <li><a href="dolacz_kod.php" class="aktualna-strona">Dołącz do testu</a></li>
+        <li><a href="moje_testy.php">Moje testy</a></li>
       </ul>
-
-
       <ul>
         <li><a href="../logowanie/logout.php">Wyloguj</a></li>
-        <li><a href="../kontakt.php" >Kontakt</a></li>
+        <li><a href="../kontakt.php">Kontakt</a></li>
       </ul>
     </div>
     <div class="main-content" id="main">
       <div class="haslo">
         <p>Dołącz do testu</p>
         <br>
-                <form method="GET" action="../test.php">
-                    <label class="full-width" for="kod">Kod testu</label>
-                    <input class="full-width" type="text" name="kod" id="kod" required>
-                    <div style="display: flex;padding-left:10%;align-items: center;"><label style="padding-left:10%;" for="zaloguj" ><a href="../kontakt.php">Problem z dołączeniem?</a></label></div>
-                    <input type="submit" value="Zatwierdź">
-                </form>
-        </div>
-        
-  </div>
+        <form method="GET" action="../test.php">
+          <label class="full-width" for="kod">Kod testu</label>
+          <input class="full-width" type="text" name="kod" id="kod" value="<?php echo $kod; ?>" required>
+          <div style="display: flex; padding-left:10%; align-items: center;">
+            <label style="padding-left:10%;" for="zaloguj">
+              <a href="../kontakt.php">Problem z dołączeniem?</a>
+            </label>
+          </div>
+          <input type="submit" value="Zatwierdź">
+        </form>
+      </div>
+    </div>
   </div>
   <div class="footer" id="stopka">
     <p>&copy; Strona testowa. Wszelkie prawa zastrzeżone.</p>
   </div>
-
 </div>
 
 <script>
-  
 var headHeight = $('#head').height() + parseInt($('#head').css('padding-top')) + parseInt($('#head').css('padding-bottom'));
 var stopkaHeight = $('#stopka').height() + parseInt($('#stopka').css('padding-top')) + parseInt($('#stopka').css('padding-bottom'));
 var menuMargin = parseInt($('#menu').css('margin-top')) + parseInt($('#menu').css('margin-bottom'));
-
-
 
 var H = headHeight + stopkaHeight + menuMargin;
 $("#menu").css('min-height', "calc(100vh - " + H + "px)");
